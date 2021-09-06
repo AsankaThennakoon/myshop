64a86a993d80163ee1ad25c5c185b823a168f467
Asanka Thennakoon <asankabandara18879@gmail.com>
2021-09-06 16:46:40 +0530

    add top popup button

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -9,7 +9,13 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
 
-    final loadedProduct = Provider.of<Products>(context).findById(productId);
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+      //if we add listen false it will not rebuild the widget here
+      //as a example here we dont need to rebuild product detail screen if any new product add time so
+      //by using this listen false we can stop rebuilding this screen.
+    ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem({required this.id, required this.title, required this.imageUrl});
+  // ProductItem({required this.id, required this.title, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
             Navigator.of(context)
-                .pushNamed(ProductDetailScreen.routName, arguments: id);
+                .pushNamed(ProductDetailScreen.routName, arguments: product.id);
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           backgroundColor: Colors.black54,
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
