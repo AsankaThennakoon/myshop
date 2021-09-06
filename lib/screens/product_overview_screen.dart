946d77cb03177b73ecefdba79f8173b1e5bbee8c
Asanka Thennakoon <asankabandara18879@gmail.com>
2021-09-06 16:38:24 +0530

    add popup filter in main screen

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
-import '../models/product.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
 import '../widgets/product_grid.dart';
+
+enum FilterOption {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [];
 
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOption selectedValue) {
+              print(selectedValue);
+              if (selectedValue == FilterOption.Favorites) {
+                //.
+                productsContainer.showFavoritesOnly();
+              } else {
+                //.
+                productsContainer.showAll();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text("Only favorites"),
+                value: FilterOption.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text("Show All"),
+                value: FilterOption.All,
+              )
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
