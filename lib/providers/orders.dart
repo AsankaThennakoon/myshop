645e289f562e180e3d488d2e56fe01c9cd4ee70f
Asanka Thennakoon <asankabandara18879@gmail.com>
2021-09-08 16:34:31 +0530

    add order now to firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:myshop/providers/cart.dart';
 
 class OrderItem {
@@ -21,14 +24,32 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = "https://myshop-99ce6-default-rtdb.firebaseio.com/orders.json";
+
+    final Uri _uri = Uri.parse(url);
+    final timestamp = DateTime.now();
+    final response = await http.post(_uri,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((e) => {
+                    'id': e.id,
+                    'title': e.title,
+                    'quantity': e.quantity,
+                    'price': e.price,
+                  })
+              .toList(),
+        }));
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
       ),
     );
     notifyListeners();
